4fdeed99db8c74a96796831bf3c26634eace7ed5
Mpinyaz <ebmpinyuri@gmail.com>
2024-03-28 16:35:14 +0200

    restructred project to do ssr

--- server/src/routes/pages/mod.rs
+++ server/src/routes/pages/mod.rs
@@ -2,7 +2,10 @@ use crate::handlers::main_handlers::*;
 use crate::models::state::AppStateType;
 use axum::routing::get;
 use axum::Router;
+use client::*;
 use hyper::Method;
+use leptos::*;
+use leptos_axum::{generate_route_list, LeptosRoutes};
 use tower_http::cors::CorsLayer;
 use tower_http::services::ServeDir;
 
@@ -25,7 +28,9 @@ pub fn init_router() -> Router<AppStateType> {
             axum::http::header::AUTHORIZATION,
             axum::http::header::ACCEPT,
         ]);
-
+    let conf = get_configuration(None).await.unwraps();
+    let leptos_options = conf.leptos_options;
+    let routes = generate_route_list(App);
     Router::new()
         .nest_service(
             "/assets",
@@ -34,5 +39,6 @@ pub fn init_router() -> Router<AppStateType> {
         .route("/", get(home))
         .route("/name/:name", get(name_handler))
         .route("/greeting", get(greeting_handler))
+        .leptos_routes(leptos_options, routes, App)
         .layer(cors)
 }
